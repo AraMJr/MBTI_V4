3c70c955bc66c03309de4306c222fcc36e7676f9
amegerdichian <aramjr@outlook.com>
2023-01-11 01:50:58 -0700

    First Commit

---
+++ function.py
@@ -0,0 +1,45 @@
+
+class Function:
+
+    def __init__(self, attrs, abbrevs):
+        self.abbrevs: dict = abbrevs
+        self.attrs: dict = attrs
+
+    def get_fullname(self, abbrev: str) -> str | None:
+        return self.attrs.get(abbrev)
+
+    def get_abbrev(self, fullname: str) -> str | None:
+        return self.abbrevs.get(fullname)
+
+    def get_opposite(self, query: str) -> str | None:
+        abbrevs = list(self.attrs.keys())
+        attrs = list(self.abbrevs.keys())
+        if query == abbrevs[0]:
+            return abbrevs[1]
+        elif query == abbrevs[1]:
+            return abbrevs[0]
+        elif query == attrs[0]:
+            return attrs[1]
+        elif query == attrs[1]:
+            return attrs[0]
+        else:
+            return None
+
+
+def instantiate(attribute1: str, abbrev1: str, attribute2: str, abbrev2: str) -> Function:
+    return Function(attrs={abbrev1: attribute1, abbrev2: attribute2},
+                    abbrevs={attribute1: abbrev1, attribute2: abbrev2})
+
+
+if __name__ == '__main__':
+    judging: Function = instantiate('thinking', 't', 'feeling', 'f')
+    print(judging)
+    print(judging.get_abbrev('thinking'))
+    print(judging.get_fullname('t'))
+    print(judging.get_opposite('t'))
+    print(judging.get_opposite('thinking'))
+    print(judging.get_opposite('intuition'))
+    print(judging.get_fullname('n'))
+
+
+
---
+++ mbti.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+import function
+
+
+class MBTI:
+
+    def __init__(self, name):
+        self.name: str = name
+        self.functions: dict | None = None
+        self.stack: dict | None = None
+        self.version: function.Function = function.instantiate('introversion', 'i', 'extroversion', 'e')
+        self.perceiving: function.Function = function.instantiate('intuition', 'n', 'sensing', 's')
+        self.judging: function.Function = function.instantiate('thinking', 't', 'feeling', 'f')
+        self.orientation: function.Function = function.instantiate('judging', 'j', 'perceiving', 'p')
+        self.populate_functions()
+        self.generate_stack()
+
+    def __str__(self):
+        return f"{self.name} -> {list(self.functions.values())}\n{self.stack}"
+
+    def populate_functions(self):
+        if self.functions is None:
+            self.functions = {}
+        v, p, j, o = tuple([*self.name])
+        self.functions['v'] = self.version.get_fullname(v)
+        self.functions['p'] = self.perceiving.get_fullname(p)
+        self.functions['j'] = self.judging.get_fullname(j)
+        self.functions['o'] = self.orientation.get_fullname(o)
+
+    def generate_stack(self):
+        if self.stack is None:
+            self.stack = {}
+
+
+
+def mbti_type(name: str) -> MBTI | None:
+    if len(name) != 4:
+        print('ERROR: INVALID TYPE (LEN NOT EQUAL TO 4)')
+        return
+    return MBTI(name=name)
+
+
+if __name__ == "__main__":
+    intp = mbti_type("intp")
+    if intp is not None:
+        print(intp)
+
